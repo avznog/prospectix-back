508d7f563b7a258c106921a06e2ae02ed438379e
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-05-15 21:33:43 +0200

    💚push avvec gitmoji et restart

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,6 +8,6 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-    //test final avec restart 
+    //testing avec gitmoji avec restart
   }
 }
