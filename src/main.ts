aa67df4a459f34837dabf01d59d3c82af81ee136
Thomas Chardo <tchardonnens@juniorisep.com>
2022-05-19 23:07:21 +0200

    📝 Added Swagger comment

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Prospectix API')
     .setDescription('Here is the API for Prospectix')
