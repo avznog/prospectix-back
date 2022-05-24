d4790fd654e3edaae2acee091fd78f741380dbd4
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-05-24 19:04:58 +0200

    ⚰️ Removed services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginService } from './services/login/login.service';
+import { GoalsService } from './services/goals/goals.service';
+import { ProspectsService } from './services/prospects/prospects.service';
+import { RemindersService } from './services/reminders/reminders.service';
+import { MeetingsService } from './services/meetings/meetings.service';
+import { UsersService } from './services/users/users.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LoginService, GoalsService, ProspectsService, RemindersService, MeetingsService, UsersService],
 })
 export class AppModule {}
