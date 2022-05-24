9bc9046f2712f5f8498e5c568777acea84f42422
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-05-24 19:21:26 +0200

    🩹 Removed dead code in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginService } from './services/login/login.service';
-import { GoalsService } from './services/goals/goals.service';
-import { ProspectsService } from './services/prospects/prospects.service';
-import { RemindersService } from './services/reminders/reminders.service';
-import { MeetingsService } from './services/meetings/meetings.service';
-import { UsersService } from './services/users/users.service';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, LoginService, GoalsService, ProspectsService, RemindersService, MeetingsService, UsersService],
+  providers: [AppService],
 })
 export class AppModule {}
