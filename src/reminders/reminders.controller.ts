92e2eff35ddf36b393d5c132611563db07de2ab8
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-06-13 17:41:30 +0200

    💥 Fixed issue with token payload

--- src/reminders/reminders.controller.ts
+++ src/reminders/reminders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
 import { RemindersService } from './reminders.service';
 import { CreateReminderDto } from './dto/create-reminder.dto';
 import { UpdateReminderDto } from './dto/update-reminder.dto';
@@ -7,6 +7,9 @@ import { Reminder } from './entities/reminder.entity';
 import { ProspectsService } from 'src/prospects/prospects.service';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from 'src/auth/services/auth.service';
+import RequestWithPm from 'src/auth/interfaces/requestWithPm.interface';
+import { ProjectManager } from 'src/project-managers/entities/project-manager.entity';
+import JwtAuthGuard from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('reminders')
 @ApiTags("reminders")
@@ -15,19 +18,21 @@ export class RemindersController {
     private readonly reminderService: RemindersService,
     ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post(":idProspect")
-  async create(@Body() createReminderDto: CreateReminderDto, @Param("idProspect") idProspect: number) : Promise<Reminder>{
+  async create(@Body() createReminderDto: CreateReminderDto, @Param("idProspect") idProspect: number, @Req() request: RequestWithPm) : Promise<Reminder>{
     // const pm = await this.pmService.findByPayload()
     // return await this.reminderService.create(pm.id, createReminderDto, idProspect);
-    return await this.reminderService.create(0, createReminderDto, idProspect);
+    request.pm = request.user as ProjectManager;
+    return await this.reminderService.create(request.pm.id, createReminderDto, idProspect);
 
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get("by-pm")
-  async findAllByPm() : Promise<Reminder[]>{
-    // const pm = await this.authService.validatePm()
-    // return await this.reminderService.findAllByPm(pm.id);
-    return await this.reminderService.findAllByPm(0);
+  async findAllByPm(@Req() request: RequestWithPm) : Promise<Reminder[]>{
+    request.pm = request.user as ProjectManager;
+    return await this.reminderService.findAllByPm(request.pm.id);
   }
 
   @Get("by-prospect/:idProspect")
