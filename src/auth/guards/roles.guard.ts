ef8a7cfcead632998ad3565d8be68953979cc7a3
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-06-19 15:02:07 +0200

    🗑️ cleaned roles guard

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,24 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
-// @Injectable()
-// export class RolesGuard implements CanActivate {
-//   constructor(private reflector: Reflector) {}
-//   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-//       const requireRoles = this.reflector.getAllAndOverride<string[]>("roles", [
-//         context.getHandler(),
-//         context.getClass(),
-//       ]);
-
-//       if(!requireRoles){
-//         return true;
-//       }
-
-//       const user: ProjectManager = {
-//         admin: 
-//       }
-//   }
-// }
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -30,27 +12,8 @@ export class RolesGuard implements CanActivate {
     if (!roles) return false;
     const request = context.switchToHttp().getRequest();
     console.log(request)
-    if(!request.user) console.log("anniversaire");
-    // const role = request.user.admin ? "Admin" : "Cdp";
+    if(!request.user) return false;
     const role = request.user.admin ? "Admin" : "Cdp";
     return roles.includes(role);
   }
 }
-
-// const RoleGuard = (role: string): Type<CanActivate> => {
-//   class RoleGuardMixin implements CanActivate {
-//     canActivate(context: ExecutionContext) {
-//         const request = context.switchToHttp().getRequest<RequestWithPm>();
-//         const user = request.user;
-//         // return request.pm?.roles.includes(role);
-//         console.log(request.pm.admin)
-//         const role = request.pm.admin ? "admin" : "cdp";
-//         // return request.pm.roles.includes(role)
-//         return request.user?.role.includes(role);
-//     }
-//   }
-
-//   return mixin(RoleGuardMixin);
-// }
-
-// export default RoleGuard;
