62c41cc4df64438ebf526b455a9a4bb54aa86f05
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-06-22 09:20:30 +0000

    ➕ Add enabling cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors();
   //Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Prospectix API')
