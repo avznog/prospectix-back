46c25d89bf2019bbeea448facb0faa2d3d2b8808
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-08-15 18:04:45 +0200

    🚧 add endpoints

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,5 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateEventDto } from './dto/create-event.dto';
+import { Event } from './entities/event.entity';
 
 @Injectable()
 export class EventsService {
+
+  constructor(
+    @InjectRepository(Event)
+    private readonly eventRepository: Repository<Event>
+  ) {}
+
+  async create(createEventDto: CreateEventDto) : Promise<Event> {
+    try {
+      return this.eventRepository.save(createEventDto);
+    } catch (error) {
+      console.log(error)
+      throw new HttpException("Impossitble de créer l'évènement", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async findAllByProspect(prospectId: number) : Promise<Event[]> {
+    try {
+      return this.eventRepository.find({
+        relations: ["pm","prospect"],
+        where: {
+          prospect: {
+            id: prospectId
+          }
+        }
+      });
+    } catch (error) {
+      console.log(error)
+      throw new HttpException("Impossible de récupérer tous les évènements pour ce prospect", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
