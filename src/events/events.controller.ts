d406938482fc670ead03008a602ccd1698dcff2c
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-08-15 17:47:48 +0200

    🔒️ add use guards to event controller

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,9 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { ApiTags } from '@nestjs/swagger';
+import JwtAuthGuard from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('events')
 @ApiTags("events")
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 }
