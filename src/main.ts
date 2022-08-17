e2297d0d7d7b3318c49e3ac3c56db9e701bbe320
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-08-17 11:52:57 +0200

    🧑‍💻 edit env var for cors / fix login issue staging

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: "http://localhost:4200",
+    origin: process.env.BASE_URL,
     credentials: true,
   })
   //Swagger configuration
