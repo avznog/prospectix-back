b550ce50fa192e616bc7c303910c8f27839be366
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-05-05 12:32:42 +0200

    🎨 fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,6 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     HttpModule,
-    // TypeOrmModule.forFeature(), // TypeOrmModule.forFeature([Slack, Website....])
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
@@ -29,39 +28,24 @@ import { UsersModule } from './users/users.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST || "localhost",
-      // host: process.env.POSTGRES_HOST || "192.168.0.158",
+      host: process.env.POSTGRES_HOST || "192.168.0.158",
       port: +process.env.POSTGRES_PORT,
       username: process.env.POSTGRES_USER || "postgres",
       password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE ?? 'prod',
-      // url: `pgsql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/prospectix`,
+      database: process.env.POSTGRES_DATABASE ?? 'dev',
       synchronize: true,
-      // entities: [SearchParams, PrimaryActivity, SecondaryActivity, Bookmark, City, Call, Country, Email, Event, GoalTemplate, Goal, MailTemplate, Meeting, NegativeAnswer, Phone, ProjectManager, Prospect, Reminder, SentEmail, Website],
-      entities: ["../src/**/*.entity.ts"]
+      autoLoadEntities: true
     }),
-    
     ScheduleModule.forRoot(),
-    
     ActionsModule,
-    
     AdminModule,
-
     ApisModule,
-    
     AuthModule,
-
     GoalsGlobalModule,
-
     MailsModule,
-
     ProspectGlobalModule,
-
     UsersModule,
-
   ],
-  // controllers: [AppController, SecondaryActivitiesController, BookmarksController, CitiesController, CallsController, CountriesController, EmailsController, EventsController, GoalTemplatesController, GoalsController, GoogleController, MailTemplatesController, MeetingsController, NegativeAnswersController, PhonesController, ProjectManagersController, ProspectsController, RemindersController, SentEmailsController, SlackController, WebsitesController, PrimaryActivityController, SearchParamsController],
-  // providers: [AppService, SecondaryActivitiesService, BookmarksService, CitiesService, CallsService, CountriesService, EmailsService, EventsService, GoalTemplatesService, GoalsService, GoogleService, MailTemplatesService, MeetingsService, NegativeAnswersService, PhonesService, ProjectManagersService, ProspectsService, RemindersService, SentEmailsService, SlackService, WebsitesService, SentryService, PrimaryActivityService, SearchParamsService],
 
 })
 export class AppModule { }
