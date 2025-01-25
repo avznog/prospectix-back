33da4e152b2f847ce7d6bb6b7c33cabd7942a77c
Benjamin Gonzva <bengonzva75@gmail.com>
2025-01-25 20:27:34 +0100

    (feat) change port

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger-api', app, document);
-  await app.listen(3000);
+  await app.listen(80);
 }
 bootstrap();
